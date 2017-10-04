236150ab29df14582a215997d3fe19cdbc0487e5
ELROY-PC\hasee <782172672@qq.com>
2017-10-04 16:49:03 -0400

    lab2_modelize_failversion

---
+++ lab2/src/__init__.py
@@ -0,0 +1,4 @@
+from .lab2_packets import *
+from .lab2_protocol import *
+from .lab2_Util import *
+from .lab2_transport import *
---
+++ lab2/src/lab2_protocol/__init__.py
@@ -0,0 +1,3 @@
+from .PassThroughProtocol import PassThroughProtocol1
+from .ClientProtocol import ClientProtocol
+from .ServerProtocol import ServerProtocol
